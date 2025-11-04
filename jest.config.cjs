a6d2232e7754a1d00f52bb11df26925c6559b79c
Amin Mousavi <tigmin@gmail.com>
2025-11-04 18:30:01 +0100

    Ensure Jest can run against ESM build outputs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
   verbose: true,
-  moduleFileExtensions: ['js', 'json', 'mjs']
+  moduleFileExtensions: ['js', 'json', 'mjs'],
+  globalSetup: './jest.setup.js'
 };
